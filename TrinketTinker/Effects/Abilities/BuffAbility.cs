479cac30f086aa47edfd979a7a149352072913e9
mushymato <wildshinobu@gmail.com>
2025-07-25 08:47:04 -0400

    add support for random buff

--- TrinketTinker/Effects/Abilities/BuffAbility.cs
+++ TrinketTinker/Effects/Abilities/BuffAbility.cs
@@ -9,13 +9,34 @@ namespace TrinketTinker.Effects.Abilities;
 public sealed class BuffAbility(TrinketTinkerEffect effect, AbilityData data, int lvl)
     : Ability<BuffArgs>(effect, data, lvl)
 {
+    private string? randAppliedBuff = null;
+
     /// <summary>Apply or refreshes the buff.</summary>
     /// <param name="proc"></param>
     /// <returns></returns>
     protected override bool ApplyEffect(ProcEventArgs proc)
     {
         // Buff(string id, string source = null, string displaySource = null, int duration = -1, Texture2D iconTexture = null, int iconSheetIndex = -1, BuffEffects effects = null, bool? isDebuff = null, string displayName = null, string description = null)
-        proc.Farmer.applyBuff(args.BuffId);
+        if (args.BuffIdList != null)
+        {
+            if (args.Mode == BuffApplyMode.All)
+            {
+                foreach (string buffId in args.BuffIdList)
+                {
+                    proc.Farmer.applyBuff(buffId);
+                }
+            }
+            else if (args.Mode == BuffApplyMode.Random)
+            {
+                int idx = Random.Shared.Next(args.BuffIdList.Count);
+                randAppliedBuff = args.BuffIdList[idx];
+                proc.Farmer.applyBuff(randAppliedBuff);
+            }
+        }
+        else
+        {
+            proc.Farmer.applyBuff(args.BuffId);
+        }
         return base.ApplyEffect(proc);
     }
 
@@ -24,7 +45,25 @@ public sealed class BuffAbility(TrinketTinkerEffect effect, AbilityData data, in
     /// <returns></returns>
     protected override void CleanupEffect(Farmer farmer)
     {
-        farmer.buffs.Remove(args.BuffId);
+        if (args.BuffIdList != null)
+        {
+            if (args.Mode == BuffApplyMode.All)
+            {
+                foreach (string buffId in args.BuffIdList)
+                {
+                    farmer.buffs.Remove(buffId);
+                }
+            }
+            else if (args.Mode == BuffApplyMode.Random)
+            {
+                farmer.buffs.Remove(randAppliedBuff);
+                randAppliedBuff = null;
+            }
+        }
+        else
+        {
+            farmer.buffs.Remove(args.BuffId);
+        }
         base.CleanupEffect(farmer);
     }
 }
